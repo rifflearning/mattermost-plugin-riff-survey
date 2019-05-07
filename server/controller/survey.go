574ac4433cc92a354adc8c54f561660180c6702c
Chetanya Kandhari <chetanya.kandhari@brightscout.com>
2019-05-08 00:14:33 +0530

    Create direct channel

--- server/controller/survey.go
+++ server/controller/survey.go
@@ -18,12 +18,17 @@ var sendSurvey = &Endpoint{
 
 func executeSendSurvey(w http.ResponseWriter, r *http.Request) error {
 	conf := config.GetConfig()
-	// userID := r.Header.Get(config.HeaderMattermostUserID)
-	channelID := r.URL.Query().Get("channel_id")
+	userID := r.Header.Get(config.HeaderMattermostUserID)
+
+	channel, appErr := config.Mattermost.GetDirectChannel(conf.BotUserID, userID)
+	if appErr != nil {
+		http.Error(w, "Error creating DM Channel.", http.StatusInternalServerError)
+		return errors.Wrap(appErr, "Error creating DM Channel.")
+	}
 
 	post := &model.Post{
 		UserId:    conf.BotUserID,
-		ChannelId: channelID,
+		ChannelId: channel.Id,
 		Type:      model.POST_DEFAULT,
 		Message:   "Sample survey post",
 		Props: model.StringInterface{
@@ -35,7 +40,7 @@ func executeSendSurvey(w http.ResponseWriter, r *http.Request) error {
 
 	if _, err := config.Mattermost.CreatePost(post); err != nil {
 		http.Error(w, "Error creating the survey post.", http.StatusInternalServerError)
-		return errors.Wrap(err, "Error creating survey post for channel: "+channelID)
+		return errors.Wrap(err, "Error creating survey post for channel: "+channel.Id)
 	}
 
 	return nil
