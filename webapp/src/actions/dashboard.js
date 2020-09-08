7612f038ce49949ffaa28bcc34a4166fc202f533
Chetanya Kandhari <chetanya.kandhari@brightscout.com>
2020-09-09 02:12:20 +0530

    MI-1005: Select meeting metrics tab by default
    - when opening metrics from survey post

--- webapp/src/actions/dashboard.js
+++ webapp/src/actions/dashboard.js
@@ -4,5 +4,8 @@ export const openRiffDashboard = (meetingID) => (dispatch) => {
     dispatch({
         type: Constants.ACTION_TYPES.OPEN_RIFF_DASHBOARD,
         meetingID,
+
+        // Always show meeting-specific metrics when showing from the survey post link
+        selectedMetrics: Constants.METRICS_TYPE_MEETING_METRICS,
     });
 };
